dce962543642ff946bed57fe17bbe06d24371f92
MD Akibur Rahman <165682667+akiburrahman97@users.noreply.github.com>
2024-08-08 15:35:07 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/* ex1 start*/
+function showText(){
+	document.getElementById('s_text').innerHTML="Hello SSB";
+}
+/* ex1 end*3
+/* ex2 start*/
+function shText(){
+	document.getElementById('a_text').innerHTML="Hello SSB";
+}
+/* ex2 end*/
+/* ex3 start*/
+function sText(){
+	document.getElementById('b_text').innerHTML="Hello SSB";
+}
+/* ex3 end*/
+/* ex4 start*/
+function howText(){
+	document.getElementById('c_text').innerHTML="Hello SSB";
+}
+/* ex4 end*/
+/* ex5 start*/
+function sawText(){
+	document.getElementById('d_text').innerHTML="Hello SSB";
+}
+/* ex5 end*/
+/* ex6 start*/
+function seenText(){
+	document.getElementById('e_text').innerHTML="Hello SSB";
+}
+/* ex6 end*/
+/* ex7 start*/
+function aText(){
+	document.getElementById('f_text').innerHTML="Hello SSB";
+}
+/* ex7 end*/
+/* ex8 start*/
+function bText(){
+	document.getElementById('g_text').innerHTML="Hello SSB";
+}
+/* ex8 end*/
+/* ex9 start*/
+function cText(){
+	document.getElementById('h_text').innerHTML="Hello SSB";
+}
+/* ex9 end*/
+/* ex10 start*/
+function dText(){
+	document.getElementById('i_text').innerHTML="Hello SSB";
+}
+/* ex10end*/
